2a8fe442775ed4a66621575b437cb984f44d9108
nfox18212 <nfox18212@gmail.com>
2025-03-13 16:22:34 -0400

    worked on adder modules

--- ksaddder.sv
+++ ksaddder.sv
@@ -2,8 +2,33 @@
     This is just the kogge-stone adder written for VLSI ported to system verilog
  */
 
- module 4bit_ksadd;
+// starting with 4 bits
+ module calculateCarryOut(a, b, cin, cout);
 
-    wire 
+    input  reg  a;
+    input  reg  b;
+    input  reg  cin;
+    output wire cout; 
+
 
  endmodule;
+
+ module sum(a, b, sum, cout);
+
+    input reg [3:0]  a;
+    input reg [3:0]  b;
+
+
+ endmodule
+
+ module calculatePG(a, b, p, g);
+
+    input reg a;
+    input reg b;
+    output wire p;
+    output wire g;
+
+    assign g = a and b;
+    assign p = a xor b;
+
+ endmodule
