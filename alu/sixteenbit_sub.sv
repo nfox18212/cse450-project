6eedc461d30003a7089df19bd2cedb546ebe3fc3
nfox18212 <nfox18212@gmail.com>
2025-03-14 19:54:49 -0400

    cleaned up sub

--- alu/sixteenbit_sub.sv
+++ alu/sixteenbit_sub.sv
@@ -1,9 +1,10 @@
 module ALU_Sixteenbit_SUB (
     input  logic [15:0] A,
     input  logic [15:0] B,
-  output logic [15:0] Answer
+    output logic [15:0] Answer
 );
-    always_comb begin
-        Answer = A - B;
-    end
+
+    assign Answer = A - B;
+
+
 endmodule
