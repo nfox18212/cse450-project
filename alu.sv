d3cb00a7cf40e5214bff0d44251b165911a0e7da
nfox18212 <nfox18212@gmail.com>
2025-03-14 19:36:19 -0400

    updated and module

--- alu.sv
+++ alu.sv
@@ -1,1 +1,5 @@
-module and(in 1, )
+module and
+#(parameter N)
+(input wire [N-1:0] a, input wire [N-1:0] b, output wire [N-1:0] c);
+    assign c = a & b;
+endmodule
--- testbench.sv
+++ testbench.sv
@@ -1,21 +1,20 @@
-// testbench to run the adder, make suer its doing things that make sense
+// testbench to run everything, make sure that things make sense
 
-module tb;
+    module tb;
+    parameter N = 4;
+    reg [N-1:0] op_1; // stands for operand 1 and 2 repsectively
+    reg [N-1:0] op_2;
 
+    wire [N-1:0] result; // result of the tested operation
 
-    
+    and #(.N(N)) and1 (.a(op_1[N-1:0]), .b(op_2), .c(result));
 
     initial begin
+
+    #1  op_1 = 'h0xA; // 0b1100
+        op_2 = 'h0xB; // 0b1101
+        $display("operand 1 is: 0x%0h, operand 2 is: 0x%0h, operation is AND so result is: 0x%0h", op_1, op_2, result);
         
-        a <= 'h0;
-        b <= 'h0;
-
-        $monitor("a = 0x%0h, b = 0x%0h, ps = 0x%0h, gs = 0x%0h", a, b, ps, gs);
-
-        #10 a <= 'h2;
-    		b <= 'h3;
-        #20 b <= 'h4;
-        #10 a <= 'h5;
 
     end   
 
